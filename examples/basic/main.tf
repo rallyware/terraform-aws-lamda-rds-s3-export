bba6e406b9710b51f7168ab31b85386b03a6b833
Pavel Kryvko <9740842+litanyofmadness@users.noreply.github.com>
2023-03-22 20:20:01 +0400

    fix: resolve some issues and add example (#1)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,6 +1,129 @@
-module "aweasome_module" {
-  source    = "../../"
-  name      = "aweasome"
-  stage     = "production"
-  namespace = "sweetops"
+resource "aws_s3_bucket" "export" {
+  bucket = "bucket-for-rds-snapshots-export"
+}
+
+resource "aws_s3_bucket_acl" "export" {
+  bucket = aws_s3_bucket.export.id
+  acl    = "private"
+}
+
+resource "aws_kms_key" "export" {}
+
+resource "aws_kms_alias" "export" {
+  name          = "alias/rds-snapshot-export-key"
+  target_key_id = aws_kms_key.export.key_id
+}
+
+data "aws_iam_policy_document" "export_assume" {
+  version = "2012-10-17"
+
+  statement {
+    sid = "ExportAssumeRole"
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type = "Service"
+      identifiers = [
+        "export.rds.amazonaws.com"
+      ]
+    }
+    effect = "Allow"
+  }
+}
+
+data "aws_iam_policy_document" "export" {
+  version = "2012-10-17"
+
+  statement {
+    sid = "AllowS3"
+    actions = [
+      "s3:PutObject*",
+      "s3:ListBucket",
+      "s3:GetObject*",
+      "s3:DeleteObject*",
+      "s3:GetBucketLocation"
+    ]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.export.id}",
+      "arn:aws:s3:::${aws_s3_bucket.export.id}/*"
+    ]
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_policy" "export" {
+  name   = "export-task-policy"
+  policy = data.aws_iam_policy_document.export.json
+}
+
+resource "aws_iam_role" "export" {
+  name               = "export-task-role"
+  assume_role_policy = data.aws_iam_policy_document.export_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "export" {
+  role       = aws_iam_role.export.name
+  policy_arn = aws_iam_policy.export.arn
+}
+
+data "aws_iam_policy_document" "lambda" {
+  version = "2012-10-17"
+
+  statement {
+    sid = "AllowExportDescribeSnapshot"
+    actions = [
+      "rds:StartExportTask",
+      "rds:DescribeDBSnapshots"
+    ]
+    resources = [
+      "*"
+    ]
+    effect = "Allow"
+  }
+
+  statement {
+    sid = "AllowGetPassRole"
+    actions = [
+      "iam:GetRole",
+      "iam:PassRole"
+    ]
+    resources = [
+      aws_iam_role.export.arn
+    ]
+    effect = "Allow"
+  }
+
+  statement {
+    sid = "AllowExportKeyKMS"
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:GenerateDataKey",
+      "kms:GenerateDataKeyWithoutPlaintext",
+      "kms:ReEncryptFrom",
+      "kms:ReEncryptTo",
+      "kms:CreateGrant",
+      "kms:DescribeKey",
+      "kms:RetireGrant",
+    ]
+    resources = [
+      aws_kms_key.export.arn
+    ]
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_policy" "lambda" {
+  name   = "export-lambda-policy"
+  policy = data.aws_iam_policy_document.lambda.json
+}
+
+module "lambda" {
+  source = "../../"
+
+  s3_bucket_id         = aws_s3_bucket.export.id
+  kms_key_id           = aws_kms_key.export.key_id
+  export_task_role_arn = aws_iam_role.export.arn
+  lambda_policy_arn    = aws_iam_policy.lambda.arn
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,39 @@
+variable "lambda_runtime" {
+  type        = string
+  default     = "python3.9"
+  description = "The runtime environment for the Lambda function you are uploading."
+}
+
+variable "lambda_architectures" {
+  type        = list(string)
+  default     = ["arm64"]
+  description = "Instruction set architecture for AWS Lambda function."
+}
+
+variable "lambda_timeout" {
+  type        = number
+  default     = 5
+  description = "The amount of time the Lambda Function has to run in seconds."
+}
+
+variable "lambda_memory" {
+  type        = number
+  default     = 128
+  description = "Amount of memory in MB the Lambda Function can use at runtime."
+}
+
+variable "lambda_description" {
+  type        = string
+  default     = "This Lambda function automates RDS snapshot export to S3"
+  description = "Description of what the Lambda Function does."
+}
+
+variable "lambda_log_retention" {
+  type        = number
+  default     = 30
+  description = "Specifies the number of days you want to retain log events in the specified log group."
+}
+
 variable "s3_bucket_id" {
   type        = string
   description = "The name of the Amazon S3 bucket to export the snapshot to"
