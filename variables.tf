c3cf1cf9486783f12d6c2c98011a7a8aafb67b7f
Pavel Kryvko <litany.of.madness@gmail.com>
2024-04-01 10:25:43 -0400

    Requested changes

--- variables.tf
+++ variables.tf
@@ -70,40 +70,41 @@ variable "s3_folder" {
 
 variable "s3_lifecycle_rules" {
   type = list(object({
-    enabled                                = optional(bool, true)
-    id                                     = optional(string)
+    enabled = optional(bool, true)
+    id      = string
+
     abort_incomplete_multipart_upload_days = optional(number)
 
+    # `filter_and` is the `and` configuration block inside the `filter` configuration.
+    # This is the only place you should specify a prefix.
     filter_and = optional(object({
-      object_size_greater_than = optional(number)
-      object_size_less_than    = optional(number)
+      object_size_greater_than = optional(number) # integer >= 0
+      object_size_less_than    = optional(number) # integer >= 1
       prefix                   = optional(string)
       tags                     = optional(map(string), {})
     }))
-
     expiration = optional(object({
-      date = optional(string)
-      days = optional(number)
+      date                         = optional(string) # string, RFC3339 time format, GMT
+      days                         = optional(number) # integer > 0
+      expired_object_delete_marker = optional(bool)
     }))
-
-    expired_object_delete_marker = optional(bool)
-
     noncurrent_version_expiration = optional(object({
-      newer_noncurrent_versions = optional(number)
-      noncurrent_days           = optional(number)
+      newer_noncurrent_versions = optional(number) # integer > 0
+      noncurrent_days           = optional(number) # integer >= 0
     }))
-
     transition = optional(list(object({
-      date          = optional(string)
-      days          = optional(number)
+      date          = optional(string) # string, RFC3339 time format, GMT
+      days          = optional(number) # integer > 0
       storage_class = optional(string)
-    })))
+      # string/enum, one of GLACIER, STANDARD_IA, ONEZONE_IA, INTELLIGENT_TIERING, DEEP_ARCHIVE, GLACIER_IR.
+    })), [])
 
     noncurrent_version_transition = optional(list(object({
-      newer_noncurrent_versions = optional(number)
-      noncurrent_days           = optional(number)
+      newer_noncurrent_versions = optional(number) # integer >= 0
+      noncurrent_days           = optional(number) # integer >= 0
       storage_class             = optional(string)
-    })))
+      # string/enum, one of GLACIER, STANDARD_IA, ONEZONE_IA, INTELLIGENT_TIERING, DEEP_ARCHIVE, GLACIER_IR.
+    })), [])
   }))
   default = [
     {
